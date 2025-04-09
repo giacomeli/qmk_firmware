795140041eb46fcad8ff14eb0521d5eb124401a4
Juliano Giacomeli <giacomeli.dev@gmail.com>
2025-04-09 20:19:48 -0300

    feat: tweeked mouse key params

--- keyboards/handwired/dactyl_manuform/config.h
+++ keyboards/handwired/dactyl_manuform/config.h
@@ -1,30 +1,16 @@
-/*
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
+#define MK_KINETIC_SPEED
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
+// Cursor movement
+#define MOUSEKEY_MOVE_DELTA            10
+#define MOUSEKEY_INTERVAL               5
+#define MOUSEKEY_DELAY                  0
+#define MOUSEKEY_INITIAL_SPEED       3000
+#define MOUSEKEY_BASE_SPEED         15000
+#define MOUSEKEY_ACCELERATED_SPEED  25000
+#define MOUSEKEY_DECELERATED_SPEED   400  // bem baixo para sniper mode
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#pragma once
-
-
-/* mouse config */
-#define MOUSEKEY_MOVE_DELTA 10
-#define MOUSEKEY_INTERVAL    16
-#define MOUSEKEY_DELAY       0
-#define MOUSEKEY_TIME_TO_MAX 10
-#define MOUSEKEY_MAX_SPEED   30
-
-/* Enables This makes it easier for fast typists to use dual-function keys */
-#define PERMISSIVE_HOLD
+// Wheel movement
+#define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS    16
+#define MOUSEKEY_WHEEL_BASE_MOVEMENTS       24
+#define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS 32
+#define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS 8
